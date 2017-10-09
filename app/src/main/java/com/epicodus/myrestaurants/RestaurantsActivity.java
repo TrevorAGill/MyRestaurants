90d936199d3b9d4e5210d2813f1c63f32df7b257
Trevor Gill <trevor.a.gill@gmail.com>
2017-10-08 19:24:33 -0700

    finish debugging

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -3,6 +3,7 @@ package com.epicodus.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,10 +11,12 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.Bind;
+
 public class RestaurantsActivity extends AppCompatActivity {
     //1. declare a new variable that will hold the layout's display text element in step 2
-    private TextView mlocationTextView;
-    private ListView mListView;
+    @Bind (R.id.locationTextView) TextView mLocationTextView;
+    @Bind (R.id.listView) ListView mListView;
     private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
             "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
             "Slappy Cakes", "Equinox", "Tilt", "Andina",
@@ -24,7 +27,7 @@ public class RestaurantsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
         //2. make the variable hold the layout's display text element, be sure to cast the element as a TextView, since that is what it was mlocationTextView was declared as
-        mlocationTextView = (TextView) findViewById(R.id.locationTextView);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
         //create the list view
         mListView = (ListView) findViewById(R.id.listView);
         //create array adapter and assign it to mListView
@@ -35,11 +38,13 @@ public class RestaurantsActivity extends AppCompatActivity {
                                              public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                                                  String restaurant = ((TextView)view).getText().toString();
                                                  Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+//                                                 Log.v("RestaurantsActivity","In the onItemClickListener");
                                              }
                                          });
                 Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         //3. change what the display text int the layout says
-        mlocationTextView.setText("Here are all the restaurants near: " + location);
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
+//        Log.d("RestaurantsActivity", "In the onCreate method!");
     }
 }
