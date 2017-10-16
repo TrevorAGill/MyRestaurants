2f068a5abf867ba3769db90db1b55c71d32563a9
Trevor Gill <trevor.a.gill@gmail.com>
2017-10-15 19:20:25 -0700

    call Yelp API and display results in console

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -10,15 +10,22 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import okhttp3.Call;
+import okhttp3.Callback;
+import java.io.IOException;
 
 import butterknife.Bind;
+import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
     //1. declare a new variable that will hold the layout's display text element in step 2
-    @Bind (R.id.locationTextView) TextView mLocationTextView;
-    @Bind (R.id.listView) ListView mListView;
-    private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food", "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar", "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
-    private String[] cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
+    @Bind(R.id.locationTextView)
+    TextView mLocationTextView;
+    @Bind(R.id.listView)
+    ListView mListView;
+    private String[] restaurants = new String[]{"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food", "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar", "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole"};
+    private String[] cuisines = new String[]{"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican"};
+    public static final String TAG = RestaurantsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +39,37 @@ public class RestaurantsActivity extends AppCompatActivity {
         MyRestaurantsArrayAdapter adapter = new MyRestaurantsArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants, cuisines);
         mListView.setAdapter(adapter);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                                             @Override
-                                             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                                 String restaurant = ((TextView)view).getText().toString();
-                                                 Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String restaurant = ((TextView) view).getText().toString();
+                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
 //                                                 Log.v("RestaurantsActivity","In the onItemClickListener");
-                                             }
-                                         });
-                Intent intent = getIntent();
+            }
+        });
+        Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         //3. change what the display text int the layout says
         mLocationTextView.setText("Here are all the restaurants near: " + location);
-//        Log.d("RestaurantsActivity", "In the onCreate method!");
+        getRestaurants(location);
+    }
+
+    private void getRestaurants(String location) {
+        final YelpService yelpService = new YelpService();
+        yelpService.findRestaurants(location, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
